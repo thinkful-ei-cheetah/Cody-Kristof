394e45edd8ca532b04436c87388c89cea3d9703d
Kristof-Pierre Cummings <kristofpierre8@gmail.com>
2019-04-02 13:16:38 -0500

    added form logic

--- views.js
+++ views.js
@@ -10,8 +10,11 @@ const HANDLER = (function(){
       console.log('hi');
     });
   };
-  const  handleFormlogic = function (){
-    console.log('form stuff');
+
+  const handleFormlogic = function() {
+    $('#dog-breed-checkbox').on('click', e=>{
+      renderForm();
+    });
   };
 
   const  fetchingDataFromApi = function (){
@@ -21,6 +24,29 @@ const HANDLER = (function(){
       .then(jsonDatat => console.log('hi'))
       .catch(error => alert('Something went wrong. Try again later.'));
   };
+
+  'use strict';
+  /*global $ */
+
+  const generateBreedForm = function(){
+    return `<label for="breed-select">What breed would you like a picture of?</label>
+ <br>
+ <br>
+ <input type="text" id="breed-select" placeholder="Husky" name="breed-select">`;
+  };
+
+  const generateAllForm = function(){
+    return `<label for="dog-count">How many pictures would you like?</label>
+  <br>
+  <br>
+  <input type="number" min="0" max="50" id="dog-count" value="3">`;
+  };
+
+  function renderForm(){
+    let formType = $('#dog-breed-checkbox').prop('checked');
+    $('.form-type').html(`${formType ? generateBreedForm() : generateAllForm()}`);
+  }
+
   function main(){
     handleSubmitButton();
     handleFormlogic();
